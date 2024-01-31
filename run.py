94c8a575733d98fb027d1b24d5e3563df5106032
Ngozi Omenka <ngozi.madu@live.se>
2024-01-31 16:49:19 +0000

    Set quiz questions

--- run.py
+++ run.py
@@ -3,8 +3,14 @@ print("Welcome to Brain Deco Quiz App")
 player = input("Do you want to play Brain Deco Quiz? ")
 
 # Check If user condition is true. If yes then play else quit
-if player != "yes":
+if player.lower() != "yes":
     quit()
 else:
     print("Okay, lets' go :)")    
 
+# Set question for user to answer 
+answer = input("What country has the highest life expectancy? ")
+if answer.lower() == "hong kong":
+    print("Correct!")
+else:
+    print("Incorrect!")    
