0e321ce25e6a6f14bb459e693ade5b463bb26af4
Ngozi Omenka <ngozi.madu@live.se>
2024-01-31 17:11:59 +0000

    Added more quiz questions for user

--- run.py
+++ run.py
@@ -9,8 +9,122 @@ else:
     print("Okay, lets' go :)")    
 
 # Set question for user to answer 
+answer = input("How many minutes are in a full week? ")
+if answer.lower() == "10,080":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What year was the United Nations established? ")
+if answer.lower() == "1945":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
 answer = input("What country has the highest life expectancy? ")
 if answer.lower() == "hong kong":
     print("Correct!")
 else:
-    print("Incorrect!")    
+    print("Incorrect!")
+
+answer = input("What disease commonly spread on pirate ships? ")
+if answer.lower() == "scurvy":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Which langauge has the more native speakers? ")
+if answer.lower() == "spanish":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Aureolin is a shade of what color ")
+if answer.lower() == "yellow":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Which planet has the most moons? ")
+if answer.lower() == "saturn":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What country has won the most World Cups ")
+if answer.lower() == "Brazil":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("In what country was Elon Musk born? ")
+if answer.lower() == "south africa":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Where is the strongest human muscle located? ")
+if answer.lower() == "jaw":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("How many hearts does an octopus have? ")
+if answer.lower() == "3":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What planet is closest to the sun? ")
+if answer.lower() == "mercury":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What phone company produced the 3310? ")
+if answer.lower() == "nokia":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What is the tallest type of tree? ")
+if answer.lower() == "redwoods":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What colors is the flag of the United Nations? ")
+if answer.lower() == "blue and white":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What country drinks the most coffee? ")
+if answer.lower() == "finland":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("Where did sushi originate? ")
+if answer.lower() == "china":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("How many dots appear on a pair of dice? ")
+if answer.lower() == "42":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("How many bones do we have in an ear? ")
+if answer.lower() == "3":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+answer = input("What city is known as The Eternal City? ")
+if answer.lower() == "rome":
+    print("Correct!")
+else:
+    print("Incorrect!")        
